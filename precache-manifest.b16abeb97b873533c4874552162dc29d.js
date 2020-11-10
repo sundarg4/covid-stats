d2cc436646818aacb4b09468d9fb96356213fd03
sundarg4 <sundarg41995@gmail.com>
2020-11-10 15:20:32 +0100

    first commit

---
+++ precache-manifest.b16abeb97b873533c4874552162dc29d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dedc3fcc0b647953d90b",
+    "url": "/covid_statistics/static/css/main.84137f5e.chunk.css"
+  },
+  {
+    "revision": "dedc3fcc0b647953d90b",
+    "url": "/covid_statistics/static/js/main.bb0595c9.chunk.js"
+  },
+  {
+    "revision": "ddc48cc5a7a002cbd0b6",
+    "url": "/covid_statistics/static/js/runtime~main.2a9a8a82.js"
+  },
+  {
+    "revision": "3ff4dd16ea4b1de39cad",
+    "url": "/covid_statistics/static/css/2.d93be24e.chunk.css"
+  },
+  {
+    "revision": "3ff4dd16ea4b1de39cad",
+    "url": "/covid_statistics/static/js/2.c879b20f.chunk.js"
+  },
+  {
+    "revision": "bb87d1c9c4ab1b79ecb2a045c1d3299d",
+    "url": "/covid_statistics/static/media/CSLOGO.bb87d1c9.png"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/covid_statistics/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/covid_statistics/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/covid_statistics/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/covid_statistics/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "596e56914ebc031de79304f587adf126",
+    "url": "/covid_statistics/index.html"
+  }
+];
